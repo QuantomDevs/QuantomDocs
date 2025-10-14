a989167e4e7788eb7ac5845716dcf562520ffc18
Snenjih <niklasoliver77@icloud.com>
2025-10-14 23:23:30 +0200

    update 1.3

--- project-informations/reference/profiles/script.js
+++
@@ -1,9 +0,0 @@
-/**
- * MAIN PAGE JAVASCRIPT
- * Funktionalität für die Terminus Hauptseite
- */
-
-// Hier können zukünftig Hauptseiten-spezifische JavaScript-Funktionen hinzugefügt werden
-// Zum Beispiel: Animationen, Interaktionen, etc.
-
-console.log('Terminus - Hauptseite geladen');
